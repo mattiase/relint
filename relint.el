7bd3405446e5eadc9af58bdf33b4d4e827371c2e
Mattias Engdegård <mattias.engdegard@gmail.com>
2026-01-12 11:58:11 +0100

    update copyright year

--- relint.el
+++ relint.el
@@ -1,6 +1,6 @@
 ;;; relint.el --- Elisp regexp mistake finder   -*- lexical-binding: t -*-
 
-;; Copyright (C) 2019-2024 Free Software Foundation, Inc.
+;; Copyright (C) 2019-2026 Free Software Foundation, Inc.
 
 ;; Author: Mattias Engdegård <mattias.engdegard@gmail.com>
 ;; Version: 2.1
